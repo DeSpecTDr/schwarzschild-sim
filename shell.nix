a2b8841120a21de2446c1a5c82f8475f90dd274a
DeSpecTDr <73001251+DeSpecTDr@users.noreply.github.com>
2022-07-17 20:54:28 +0300

    Initial commit

---
+++ shell.nix
@@ -0,0 +1,15 @@
+{ pkgs ? import <nixpkgs> { } }:
+let
+  python-with-my-packages = pkgs.python3.withPackages (p: with p; [
+    jupyter
+    ipykernel
+    ipympl
+    matplotlib
+    numpy
+    scipy
+    astropy
+    pandas
+    autopep8
+  ]);
+in
+python-with-my-packages.env
